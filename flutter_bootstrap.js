702d0f473a1c6646deec80e9ae78845112fcea2c
bunny1v <tom7san7@gmail.com>
2025-05-22 12:10:25 +0530

    updated new

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2494728267"
+    serviceWorkerVersion: "3259083147"
   }
 });
