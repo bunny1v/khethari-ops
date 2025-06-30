158d6b182a0b0549e0fc5fe98141f39142a27704
bunny1v <tom7san7@gmail.com>
2025-06-30 16:07:07 +0530

    error solved home page

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "741591852"
+    serviceWorkerVersion: "3351839103"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -29,14 +29,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "8902204527f5105b614a85520345f3b4
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "c36d77f27d8bff1563165647b9729d63",
+"flutter_bootstrap.js": "a6157d8c620aab221a566203ed2d7b83",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "9fdcbb954de3d8022d025a7f30080673",
 "/": "9fdcbb954de3d8022d025a7f30080673",
-"main.dart.js": "2893edbe7b15c665a2c3c2503640df49",
+"main.dart.js": "2514e7ab13c590c22db40e6e587b9320",
 "manifest.json": "f4756088f6f23e8c94485c03c0fddc7f",
 "version.json": "c89bf570ffd3ab85f6757080d1ac100d"};
 // The application shell files that are downloaded before a service worker can
